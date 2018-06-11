50e294642cd86b3db1ae1f608ffafef0e53504f1
chrissydc <bychriscelle@gmail.com>
2018-06-10 23:28:43 -0400

    pseudocode in game.js added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,19 @@
 //shown a random number at the start of the game
 
+$(document).ready(function()) {
+    // show random number at the start of game from 19 to 120
 
-document.onkeyup = function (event) {
+    // each image has its own random value from 1 to 12
+
+    // update total number every time each image is clicked
     
+    // player wins if total score = random score
+
+    // player loses if total score > random score
+
+    // game restarts when player win++ or loses++
+
+    // new game = new random number & new crystal values & total score = 0
+
+    // show wins and losses without refreshing page when restarting game
 }

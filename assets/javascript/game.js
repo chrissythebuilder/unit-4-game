8744de402a100911b34515b7ab41dec0e9106372
chrissydc <bychriscelle@gmail.com>
2018-06-19 23:14:38 -0400

    game restarts and message shows

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,25 @@ var randomNumber = Math.floor(Math.random() * (120 - 19 + 1))+ 19;
 // show random number at the start of game from 19 to 120
 $("#randNumber").html("<strong>" + randomNumber + "</strong>");
 
+var finalscore = {
+    final: function() {
+        if (totalscore === randomNumber) {
+        $("#text").html("<strong> Yay! You won! </strong> <br />");
+        wins++;
+    // game restarts when player win++ or loses++
+        randomNumber = [];
+        totalscore = [];
+    };
+    if (totalscore > randomNumber) {
+        loss++;
+    // game restarts when player win++ or loses++
+        $("#text").html("<strong> Sorry, try again! </strong> <br />");
+        randomNumber = [];
+        totalscore = [];
+    }
+    }
+};
+
 // each image has its own random value from 1 to 12    
 // statement, does not return anything
     $("#img1").attr("data-value", Math.floor(Math.random() * 12) + 1);
@@ -29,6 +48,7 @@ $("#randNumber").html("<strong>" + randomNumber + "</strong>");
         console.log($(this).data('value'));
         totalscore = parseInt(totalscore);
         $("#totalscore").html("<strong>" + totalscore + "</strong> <br>");
+        finalscore.final();
     });
 
     $("#img2").on("click", function () {
@@ -36,6 +56,7 @@ $("#randNumber").html("<strong>" + randomNumber + "</strong>");
         console.log($(this).data('value'));        
         totalscore = parseInt(totalscore);
         $("#totalscore").html("<strong>" + totalscore + "</strong>");
+        finalscore.final();
     });
 
     $("#img3").on("click", function () {
@@ -43,6 +64,7 @@ $("#randNumber").html("<strong>" + randomNumber + "</strong>");
         console.log($(this).data('value'));
         totalscore = parseInt(totalscore);
         $("#totalscore").html("<strong>" + totalscore + "</strong>");
+        finalscore.final();
     });
 
     $("#img4").on("click", function () {
@@ -50,28 +72,31 @@ $("#randNumber").html("<strong>" + randomNumber + "</strong>");
         console.log($(this).data('value'));
         totalscore = parseInt(totalscore);
         $("#totalscore").html("<strong>" + totalscore + "</strong>");
+        finalscore.final();
     });
 
+
+
     // update total number every time each image is clicked
-    var total = $("#totalscore");
-    var randomNumber = $("#randNumber");
-    if (total === randomNumber) {
-        $("#text").html("<strong> Yay! You won! </strong> <br />");
-        wins++;
-    // game restarts when player win++ or loses++
-        randomNumber = [];
-        totalscore = [];
-    } 
+    // var total = $("#totalscore");
+    // var randomNumber = $("#randNumber");
+    // if (total === randomNumber) {
+    //     $("#text").html("<strong> Yay! You won! </strong> <br />");
+    //     wins++;
+    // // game restarts when player win++ or loses++
+    //     randomNumber = [];
+    //     totalscore = [];
+    // } 
 
-    // player loses if total score > random score
+    // // player loses if total score > random score
 
-    if (total > randomNumber) {
-        loss++;
-    // game restarts when player win++ or loses++
-        $("#text").html("<strong> Sorry, try again! </strong> <br />");
-        randomNumber = [];
-        totalscore = [];
-    }
+    // if (total > randomNumber) {
+    //     loss++;
+    // // game restarts when player win++ or loses++
+    //     $("#text").html("<strong> Sorry, try again! </strong> <br />");
+    //     randomNumber = [];
+    //     totalscore = [];
+    // }
 
     // player wins if total score = random score
 

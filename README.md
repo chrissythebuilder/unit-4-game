2b3f198466b5dd745434cc3b2a8d547832d8df2e
chrissydc <bychriscelle@gmail.com>
2018-07-22 16:57:48 -0400

    read me file updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# unit-4-game
+# Crystal Bonanza
+
+Description: A one-player game where the objective is to match the final score to the random score that was generated. Each crystal on the page has a random number attached to it so the user has to figure out what the value of each crystal is to help them get to the score they desire to attain. If the score matches, the user gets a winning point and if the user exceeds the random number then the user gets a losing point. 
+
+Technologies: HTML, CSS, jQuery
+
+Github Live: https://chrissythebuilder.github.io/unit-4-game/
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,21 @@
 //shown a random number at the start of the game
 
-var wins = 0;
-var loss = 0;
-var totalscore = 0;
+
 
 
 $(document).ready(function() {
 
-var randomNumber = Math.floor(Math.random() * (120 - 19 + 1))+ 19;
+    var wins = 0;
+    var loss = 0;
+    var totalscore = 0;
 
-// show random number at the start of game from 19 to 120
-$("#randNumber").html("<strong>" + randomNumber + "</strong>");
+    var randomNumber = Math.floor(Math.random() * (120 - 19 + 1))+ 19;
 
-var finalscore = {
-    final: function() {
-        if (totalscore === randomNumber) {
-        $("#text").html("<strong> Yay! You won! </strong> <br />");
-        wins++;
-    // game restarts when player win++ or loses++
-        randomNumber = [];
-        totalscore = [];
-    };
-    if (totalscore > randomNumber) {
-        loss++;
-    // game restarts when player win++ or loses++
-        $("#text").html("<strong> Sorry, try again! </strong> <br />");
-        randomNumber = [];
-        totalscore = [];
-    }
-    }
-};
+    // show random number at the start of game from 19 to 120
+    $("#randNumber").html("<strong>" + randomNumber + "</strong>");
 
-// each image has its own random value from 1 to 12    
-// statement, does not return anything
+    // each image has its own random value from 1 to 12    
+    // statement, does not return anything
     $("#img1").attr("data-value", Math.floor(Math.random() * 12) + 1);
 
     $("#img2").attr("data-value", Math.floor(Math.random() * 12) + 1);
@@ -41,40 +24,66 @@ var finalscore = {
 
     $("#img4").attr("data-value", Math.floor(Math.random() * 12) + 1);
 
+    var finalscore = function () {
+        if (totalscore === randomNumber) {
+        $("#text").html("<strong> Yay! You won! </strong> <br />");
+        wins++;
+        $("#wins").html("<b>" + wins + "</b>")
+        // game restarts when player win++ or loses++
+        randomNumber = [];
+        totalscore = [];
+        return finalscore;
+        
+            };
+        if (totalscore > randomNumber) {
+        loss++;
+        $("#loss").html("<b>" + loss + "</b>")
+        // game restarts when player win++ or loses++
+        $("#text").html("<strong> Sorry, try again! </strong> <br />");
+        randomNumber = [];
+        totalscore = [];
+        return finalscore;
+        
+            };
+    }  
 
-    $("#img1").on("click", function () {
-        // $(this).data('value') = action to gen a value
-        totalscore = totalscore + $(this).data('value');
-        console.log($(this).data('value'));
-        totalscore = parseInt(totalscore);
-        $("#totalscore").html("<strong>" + totalscore + "</strong> <br>");
-        finalscore.final();
-    });
 
-    $("#img2").on("click", function () {
-        totalscore = totalscore + $(this).data('value');
-        console.log($(this).data('value'));        
-        totalscore = parseInt(totalscore);
-        $("#totalscore").html("<strong>" + totalscore + "</strong>");
-        finalscore.final();
-    });
+    var imageClick = function () {
 
-    $("#img3").on("click", function () {
-        totalscore = totalscore + $(this).data('value');
-        console.log($(this).data('value'));
-        totalscore = parseInt(totalscore);
-        $("#totalscore").html("<strong>" + totalscore + "</strong>");
-        finalscore.final();
-    });
+        $("#img1").on("click", function () {
+            // $(this).data('value') = action to gen a value
+            totalscore = totalscore + $(this).data('value');
+            console.log($(this).data('value'));
+            totalscore = parseInt(totalscore);
+            $("#totalscore").html("<strong>" + totalscore + "</strong> <br>");
+            finalscore();
+        });
 
-    $("#img4").on("click", function () {
-        totalscore = totalscore + $(this).data('value');
-        console.log($(this).data('value'));
-        totalscore = parseInt(totalscore);
-        $("#totalscore").html("<strong>" + totalscore + "</strong>");
-        finalscore.final();
-    });
+        $("#img2").on("click", function () {
+            totalscore = totalscore + $(this).data('value');
+            console.log($(this).data('value'));        
+            totalscore = parseInt(totalscore);
+            $("#totalscore").html("<strong>" + totalscore + "</strong>");
+            finalscore();
+        });
 
+        $("#img3").on("click", function () {
+            totalscore = totalscore + $(this).data('value');
+            console.log($(this).data('value'));
+            totalscore = parseInt(totalscore);
+            $("#totalscore").html("<strong>" + totalscore + "</strong>");
+            finalscore();
+        });
+
+        $("#img4").on("click", function () {
+            totalscore = totalscore + $(this).data('value');
+            console.log($(this).data('value'));
+            totalscore = parseInt(totalscore);
+            $("#totalscore").html("<strong>" + totalscore + "</strong>");
+            finalscore();
+        });
+    };
+    imageClick();
 
 
     // update total number every time each image is clicked
